287067b28dd9db3cca5bc2840a5fccdd69e80ef4
TapuCosmo <tapucosmo@gmail.com>
2020-11-08 18:48:56 -0800

    Edit config to include all pins required

--- matlab/script.m
+++ matlab/script.m
@@ -2,4 +2,8 @@ clear all;
 
 config = jsondecode(fileread("../config.json")).matlab;
 
-ard = arduino(config.arduinoPort, config.arduinoBoard);
+ard = arduino(config.arduino.port, config.arduino.board);
+
+configurePin(ard, config.arduino.pins.photoresistor, "AnalogInput");
+configurePin(ard, config.arduino.pins.redLED, "DigitalOutput");
+configurePin(ard, config.arduino.pins.irLED, "DigitalOutput");
